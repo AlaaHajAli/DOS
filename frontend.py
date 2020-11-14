bf0d7feca406980900003805b29bd632832d7a67
AlaHajAli <hajali.alaa98@gmail.com>
2020-11-14 13:02:13 +0200

    naming ip adresses for easier usage

--- frontend.py
+++ frontend.py
@@ -3,6 +3,10 @@ from flask import Flask
 from flask_restful import Api, Resource, request
 
 
+### INTIALIZIN IP ADDRESSES of each server
+CATALOG = 'http://192.168.1.113:5000'
+LOG = 'http://192.168.1.116:5000'
+
 ### initializing the service as an api
 app = Flask(__name__)
 api = Api(app)
@@ -10,34 +14,34 @@ api = Api(app)
 ### handling the log server requests.
 class LogHandler(Resource):
     def post(self, book_id):
-        ### directing the client's request to the correct server and returning the response -same for the rest-
-        response = requests.post('http://192.168.1.116:5000/buy/'+str(book_id))
+        ### directing the client's request to the correct server and returning the response -same for the request-
+        response = requests.post(LOG+'/buy/'+str(book_id))
         return response.json()
 
 ### handling the catalog server requests.
 class CatalogHandler(Resource): 
     def put(self, book_id):
         args = request.args
-        response = requests.put('http://192.168.1.113:5000/add/'+str(book_id), args)
+        response = requests.put(CATALOG+'/add/'+str(book_id), args)
         return response.json()
 
     def patch(self, book_id):
         args = request.args
-        response = requests.patch('http://192.168.1.113:5000/update/'+str(book_id), args)
+        response = requests.patch(CATALOG+'/update/'+str(book_id), args)
         return response.json()
     
     def get(self, book_id):
-        response = requests.get('http://192.168.1.113:5000/lookup/'+str(book_id))
+        response = requests.get(CATALOG+'/lookup/'+str(book_id))
         return response.json()
 
 class CatalogHandlerSearch(Resource): 
     def get(self):
-        response = requests.get('http://192.168.1.113:5000/search')
+        response = requests.get(CATALOG+'/search')
         return response.json()
 
 class CatalogHandlerSearchTopic(Resource): 
     def get(self, book_topic):
-        response = requests.get('http://192.168.1.113:5000/search/'+str(book_topic))
+        response = requests.get(CATALOG+'/search/'+str(book_topic))
         return response.json()
 
 api.add_resource(LogHandler, '/buy/<int:book_id>')
--- log.py
+++ log.py
@@ -2,6 +2,9 @@ import requests
 from flask import Flask
 from flask_restful import Api, Resource
 
+### IP ADDRESS OF CATALOG SERVER
+CATALOG = 'http://192.168.1.113:5000'
+
 ### initializing the service as an api
 app = Flask(__name__)
 api = Api(app)
@@ -10,7 +13,7 @@ class Log(Resource):
     ### only one method for log to handle buying a book
     def post(self, book_id):
         ### first contact catalog server to be sure that the book exists and is in stock
-        response = requests.get('http://192.168.1.113:5000/lookup/'+str(book_id))
+        response = requests.get(CATALOG+'/lookup/'+str(book_id))
         if response.json().get('message') == 'Could not find book with that id':
             return(response.json().get('message'))
         elif response.json().get('stock_count') == 0:
@@ -18,7 +21,7 @@ class Log(Resource):
         else:
             val = response.json().get('stock_count') - 1
             ### if exists and in stock, contact catalog to update the book's stock count
-            resp = requests.patch('http://192.168.1.113:5000/update/'+str(book_id), {'stock_count': val})
+            resp = requests.patch(CATALOG+'/update/'+str(book_id), {'stock_count': val})
             return("Bought Book : " + str(resp.json().get('name')))
 
 api.add_resource(Log, "/buy/<int:book_id>")
